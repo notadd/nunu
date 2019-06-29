ea11773cd2d0d26f92d83f955e90ee294d7d90f3
imeepos <1037483576@qq.com>
2019-06-29 17:55:15 +0800

    feat: 🎸 create middleware

--- packages/fastify/lib/fastify.ts
+++ packages/fastify/lib/fastify.ts
@@ -1,10 +1,23 @@
-import { Plugin, FastifyInstance, FastifyError } from 'fastify';
+import { Plugin, FastifyInstance, FastifyError, Middleware } from 'fastify';
 import * as http from 'http'
 export interface NunuOptions { }
-export function createNunu<HttpServer = http.Server, HttpRequest = http.IncomingMessage, HttpResponse = http.ServerResponse, T = any>(
+/**
+ * 插件
+ * @param options 
+ */
+export function createPlugin<HttpServer = http.Server, HttpRequest = http.IncomingMessage, HttpResponse = http.ServerResponse, T = any>(
     options: NunuOptions
 ): Plugin<HttpServer, HttpRequest, HttpResponse, T> {
     return (instance: FastifyInstance<HttpServer, HttpRequest, HttpResponse>, opts: T, callback: (err?: FastifyError) => void) => {
-        
+        // todo
+    }
+}
+/**
+ * 中间件
+ * @param options 
+ */
+export function createMiddleware<HttpServer = http.Server, HttpRequest = http.IncomingMessage, HttpResponse = http.ServerResponse>(options: NunuOptions): Middleware<HttpServer, HttpRequest, HttpResponse> {
+    return (req: HttpRequest, res: HttpResponse, callback: (err?: FastifyError) => void) => {
+        // todo
     }
 }
